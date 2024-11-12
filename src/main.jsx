1b6d6ca01c2fe48ef0c432ad3b30c30bd41dea8b
leo_WSL <leoschonberger@gmail.com>
2024-11-12 14:23:35 -0800

    Removed node_modules and cleaned up packages

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { NotificationsProvider } from "@mantine/notifications"
-import { MantineProvider } from "@mantine/core";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
